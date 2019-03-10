3e90ff9ff5b7a650a3535be5a9870f5e89455929
saber-nyan <saber-nyan@ya.ru>
2019-03-10 18:47:34 +0300

    Модели Post, Thread; админка

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import File, Post, Thread
+
+
+def resolution(obj):
+    return f'{obj.width}x{obj.height}'
+
+
+class FileMembershipInline(admin.StackedInline):
+    model = Post.files.through
+
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    inlines = [
+        FileMembershipInline,
+    ]
+    list_display = ('hash', 'filename', resolution, 'size', 'filetype', 'created_at', 'modified_at',)
+    list_filter = ('filetype',)
+    search_fields = ['hash', 'filename', ]
+    save_on_top = True
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        FileMembershipInline,
+    ]
+    list_display = ('post_id', 'banned', 'warned', 'email', 'name', 'created_at', 'modified_at',)
+    list_filter = ('banned', 'warned', 'op',)
+    search_fields = ['post_id', 'text', 'email', 'name', 'subject', 'trip_code', 'files__filename', 'files__hash']
+    save_on_top = True
+
+
+class PostInline(admin.StackedInline):
+    model = Post
+
+
+@admin.register(Thread)
+class ThreadAdmin(admin.ModelAdmin):
+    inlines = [
+        PostInline,
+    ]
+    list_filter = ('posts__banned', 'posts__warned', 'posts__op',)
+    search_fields = ['posts__post_id', 'posts__text', 'posts__email', 'posts__name', 'posts__subject',
+                     'posts__trip_code', 'posts__files__filename', 'posts__files__hash']
--- api/models.py
+++ api/models.py
@@ -5,11 +5,15 @@ import os
 from enum import Enum
 
 from django.db import models
+from django.utils import timezone
 
 from gchan import settings
 
 
 class File(models.Model):
+    class Meta:
+        ordering = ['-created_at', '-modified_at', 'filename', ]
+
     class FileTypeEnum(Enum):
         JPEG = 'jpeg'
         PNG = 'png'
@@ -17,16 +21,67 @@ class File(models.Model):
         WEBM = 'webm'
         WEBP = 'webp'
 
-    filename = models.CharField('Media file name', max_length=256)
-    width = models.PositiveIntegerField('Media file width')
-    height = models.PositiveIntegerField('Media file height')
-    size = models.PositiveIntegerField('Media file size')
-    path = models.FilePathField('Media file path (fullres)', path=os.path.join(settings.USER_FILE_PATH, 'fullres'),
+    hash = models.CharField('SHA512', max_length=128, primary_key=True)
+    filename = models.CharField('file name', max_length=256)
+    width = models.PositiveIntegerField('media width')
+    height = models.PositiveIntegerField('media height')
+    size = models.PositiveIntegerField('file size')
+    path = models.FilePathField('file path (fullres)', path=os.path.join(settings.USER_FILE_PATH, 'fullres'),
                                 max_length=500)
-    thumbnailPath = models.FilePathField('Media file path (thumbnail)',
+    thumbnailPath = models.FilePathField('file path (thumbnail)',
                                          path=os.path.join(settings.USER_FILE_PATH, 'thumb'), max_length=500)
-    thumbnailWidth = models.PositiveIntegerField('Media file width (thumbnail)')
-    thumbnailHeight = models.PositiveIntegerField('Media file height (thumbnail)')
-    filetype = models.CharField('Media file type', max_length=4,
+    thumbnailWidth = models.PositiveIntegerField('file width (thumbnail)')
+    thumbnailHeight = models.PositiveIntegerField('file height (thumbnail)')
+    filetype = models.CharField('file type', max_length=4,
                                 choices=[(tag.name, tag.value) for tag in FileTypeEnum])
-    hash = models.CharField('SHA512 file hash', max_length=128)
+
+    created_at = models.DateTimeField('created at', editable=False)
+    modified_at = models.DateTimeField('modified at', blank=True, editable=False)
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        if not self.created_at:
+            self.created_at = timezone.now()
+        self.modified_at = timezone.now()
+        return super().save(force_insert, force_update, using, update_fields)
+
+    def __str__(self):
+        return f'{self.filename} <{self.hash}> {self.width}x{self.height}'
+
+
+class Post(models.Model):
+    class Meta:
+        ordering = ['post_id', ]
+
+    thread = models.ForeignKey('Thread', on_delete=models.CASCADE, related_name='posts',
+                               verbose_name='thread', null=False, blank=False)
+
+    post_id = models.PositiveIntegerField('post ID')
+    banned = models.BooleanField('banned')
+    warned = models.BooleanField('warned')
+    text = models.TextField('text')
+    email = models.CharField('email', max_length=64, blank=True)
+    name = models.CharField('name', max_length=64, blank=True)
+    subject = models.CharField('subject', max_length=64, blank=True)
+    trip_code = models.CharField('tripcode', max_length=64, blank=True)
+    op = models.BooleanField('OP mark')
+
+    files = models.ManyToManyField(File, verbose_name='files', blank=True)
+
+    created_at = models.DateTimeField('created at', editable=False)
+    modified_at = models.DateTimeField('modified at', blank=True, editable=False)
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        if not self.created_at:
+            self.created_at = timezone.now()
+        self.modified_at = timezone.now()
+        return super().save(force_insert, force_update, using, update_fields)
+
+    def __str__(self):
+        return f'>>{self.post_id} from "{self.name}"'
+
+
+class Thread(models.Model):
+    def __str__(self):
+        return f'#{self.pk} ({self.posts.first()})'
